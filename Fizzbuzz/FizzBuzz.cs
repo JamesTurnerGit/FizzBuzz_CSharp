4e284680fe7999c6c07276f3cde470d0f966eb10
James Turner <mail.jt@gmail.com>
2017-04-14 18:18:38 +0100

    first commit

---
+++ Fizzbuzz/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    public class FizzBuzz
+    {
+        public static string FizzBuzzInt(int num)
+        {
+            if (IsDivisorOfThree(num) && IsDivisorOfFive(num)) { return "FizzBuzz"; }
+            if (IsDivisorOfThree(num)) { return "Fizz"; }
+            if (IsDivisorOfFive(num)) { return "Buzz"; }
+            return num.ToString();
+        }
+
+        private static bool IsDivisorOfThree(int num)
+        {
+            return num % 3 == 0;
+        }
+
+        private static bool IsDivisorOfFive(int num)
+        {
+            return num % 5 == 0;
+        }
+    }
+}
---
+++ FizzbuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fizzbuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+
+        [TestMethod()]
+        public void FizzBuzzNotADivisorOfThreeOrFive()
+        {
+            var actual = FizzBuzz.FizzBuzzInt(2);
+
+            Assert.AreEqual("2", actual);
+        }
+
+        [TestMethod()]
+        public void FizzBuzzDisvisorOfThree()
+        {
+            var actual = FizzBuzz.FizzBuzzInt(3);
+
+            Assert.AreEqual("Fizz", actual);
+        }
+
+        [TestMethod()]
+        public void FizzBuzzDisvisorOfFive()
+        {
+            var actual = FizzBuzz.FizzBuzzInt(5);
+
+            Assert.AreEqual("Buzz", actual);
+        }
+
+        [TestMethod()]
+        public void FizzBuzzDisvisorOfThreeAndFive()
+        {
+            var actual = FizzBuzz.FizzBuzzInt(15);
+
+            Assert.AreEqual("FizzBuzz", actual);
+        }
+
+    }
+}
